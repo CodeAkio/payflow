1f183e97c52676a26bdc4071dc293a6682f0018f
CodeAkio <siqueira_2004@hotmail.com>
2021-09-10 17:09:53 -0300

    Fix error that close insert boleto page after some time

--- lib/modules/barcode_scanner/barcode_scanner_controller.dart
+++ lib/modules/barcode_scanner/barcode_scanner_controller.dart
@@ -11,7 +11,7 @@ class BarcodeScannerController {
   BarcodeScannerStatus get status => statusNotifier.value;
   set status(BarcodeScannerStatus status) => statusNotifier.value = status;
 
-  final barcodeScanner = GoogleMlKit.vision.barcodeScanner();
+  var barcodeScanner = GoogleMlKit.vision.barcodeScanner();
 
   InputImage? inputPicker;
   CameraController? cameraController;
@@ -70,7 +70,7 @@ class BarcodeScannerController {
   void listenCamera() {
     if (cameraController!.value.isStreamingImages == false)
       cameraController!.startImageStream((cameraImage) async {
-        if (status.stopScanner) {
+        if (status.stopScanner == false) {
           try {
             final WriteBuffer allBytes = WriteBuffer();
 
